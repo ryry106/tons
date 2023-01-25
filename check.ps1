8cc16f05ec60078bcaeaccf5dff803b4771d994c
ryry106 <ryrymrs9@gmail.com>
2023-01-25 23:50:21 +0900

    update

--- check.ps1
+++ check.ps1
@@ -1,9 +1,32 @@
+Param(
+    [Parameter(Mandatory=$true)] [string] $d_or_u,
+    [string] $searchKeyword
+)
+
 $config = @{
   target = "misc"
 }
 
-foreach ($obj in (get-childitem -Path $config.target))
-{
-  select-string -path $obj.fullname -pattern "^-.*"
+function get-searchStatusExpr{
+  param(
+    [string] $d_or_u
+  )
+  process {
+    $res = "^-.*"
+    if ($d_or_u -eq "d") {
+      $res = "^x.*"
+    }
+    return $res
+  }
 }
 
+$searchStatusExpr = get-searchStatusExpr $d_or_u
+foreach ($obj in (get-childitem -Path $config.target))
+{
+  $res = select-string -path $obj.fullname -pattern $searchStatusExpr
+  if ($searchKeyword -ne "") {
+    $res = $res | where {$_.line -like "*${searchKeyword}*" -or $_.filename -like "*${searchKeyword}*"}
+  }
+}
+
+$res
