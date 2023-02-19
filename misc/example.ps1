702361054d8bcb4ece460aa19612c61eff597630
ryry106 <ryrymrs9@gmail.com>
2023-02-19 22:44:47 +0900

    update example

--- misc/example.ps1
+++ misc/example.ps1
@@ -3,10 +3,19 @@
 "this,is,a,pen".split(",")[1]
 
 # filter
-./misc/example-obj.ps1 | where { $_.head -eq "h2" }
+../tests/example-obj.ps1 | where { $_.head -eq "h2" }
 
 # map
-./misc/example-obj.ps1 | foreach { $_.body + "add" }
+../tests/example-obj.ps1 | foreach { $_.body + "add" }
 
 # file hash
-(get-filehash gantt.ps1).hash
+(get-filehash tableparser.html).hash
+
+# date format
+$18 = [DateTime]::ParseExact("02/18","MM/dd", $null);
+$19 = [DateTime]::ParseExact("02/19","MM/dd", $null);
+
+# date compare
+if ($18.CompareTo($19) -lt 0) { echo "比較元 < 比較先 の場合は負の値" }
+if ($19.CompareTo($18) -gt 0) { echo "比較元 > 比較先 の場合は正の値" }
+if ($19.CompareTo($19) -eq 0) { echo "比較元 = 比較先 の場合は0" }
